6717a53f3895b0510051396271a32bb7f82fde6a
Ashley Tyrer <au699132@uni.au.dk>
2023-01-29 00:12:39 +0100

    Edited compute_accuracy

--- HBM_Ashley_main_script_sims_1.py
+++ HBM_Ashley_main_script_sims_1.py
@@ -31,8 +31,8 @@ def compute_accuracy(y_true: np.ndarray, y_pred: np.ndarray) -> np.ndarray:
     num_trials, num_timepoints = y_true.shape
     r2_scores: np.ndarray = np.zeros(num_timepoints)
     # iterating over every label and checking it with the true sample
-    for timepoint, (actual, predicted) in enumerate(zip(y_true, y_pred)):
-        r2_scores[timepoint] = r2_score(y_true, y_pred)
+    for timepoint, (actual, predicted) in enumerate(zip(y_true.T, y_pred.T)):
+        r2_scores[timepoint] = r2_score(actual, predicted)
     return r2_scores
 
 
